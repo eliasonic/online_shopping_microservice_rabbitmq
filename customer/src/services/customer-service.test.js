33e10779041b29805268c303b90b7540b976c366
Elias Adjetey <eliasonics@gmail.com>
2024-04-24 23:20:16 +0000

    feat-10: new feature

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,7 @@
 describe('CustomerService', () => {
 
     describe('SignIn', () => {
+        // Which scenarios we are testing
         test('Validate user inputs', () => {})
 
         test('Validate response', async () => {})
