4ef71421910a6a7aafa8a26f22ce9d3fbfd2477e
Elias Adjetey <eliasadjetey@hotmail.com>
2024-04-24 23:46:54 +0000

    feat-10: new feature (#1)
    * feat-10: new feature
    
    * feat-10: updated test
    
    ---------
    
    Co-authored-by: Elias Adjetey <eliasonics@gmail.com>

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,7 @@
 describe('CustomerService', () => {
-
+    // Which function we are testing
     describe('SignIn', () => {
+        // Which scenarios we are testing
         test('Validate user inputs', () => {})
 
         test('Validate response', async () => {})
